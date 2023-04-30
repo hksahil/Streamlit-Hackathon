4d06942194733bc54eacfd276193fc976b62ab79
Sahil-choudhary <32207288+hksahil@users.noreply.github.com>
2023-04-30 15:43:53 +0530

    Added boilerplate for snowflake streamlit connection
    Notes:
    > Snowpark requires 3.8 version so create a conda env of same version

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+import streamlit as st
+from snowflake.snowpark import Session
 
+st.title('Layoffs Tracker')
+
+# Snowflake Connection
+@st.cache_resource
+def create_session():
+    return Session.builder.configs(st.secrets.snowflake).create()
+
+session = create_session()
+
+# Table Loader
+@st.cache_data
+def snowflake_loader(table_name):
+    table = session.table(table_name)
+    table = table.collect()
+    return table
+
+# Table name in Snowflake
+table_name = "STREAMLIT_DEMO.STREAMLIT.LAYOFFS"
+
+# Displaying data
+df = snowflake_loader(table_name)
+st.dataframe(df)
